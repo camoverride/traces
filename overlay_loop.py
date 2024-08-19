5207b9c07067435e92f3a7f888eae3de5ae5076b
cam <cam@cams-MacBook-Pro.local>
2024-08-19 14:12:55 -0700

    resize capture, not frame

--- overlay_loop.py
+++ overlay_loop.py
@@ -10,47 +10,43 @@ from picamera2 import Picamera2
 
 
 
-def capture_frames(frame_count, width, height):
+# Initialize Picamera2 once
+picam2 = Picamera2()
+
+def capture_frames(picam2, frame_count, width, height):
     """
     Capture a series of frames.
     """
-    picam2 = Picamera2()
     picam2.configure(picam2.create_still_configuration(main={"size": (width, height), "format": "RGB888"}))
     picam2.start()
     frames = []
     for _ in range(frame_count):
         frame = picam2.capture_array()
         frames.append(frame)
-
     picam2.stop()
 
     return np.array(frames)
 
-
-def alpha_blend_frames(new_frames, current_frames, alpha):
+def alpha_blend_frames(new_frames, current_composite_frames, alpha):
     """
     Alpha blend two images.
     """
     blended_frames = []
     for i in range(new_frames.shape[0]):
-        blended_frame = cv2.addWeighted(new_frames[i], alpha, current_frames[i], 1 - alpha, 0)
+        blended_frame = cv2.addWeighted(new_frames[i], alpha, current_composite_frames[i], 1 - alpha, 0)
         blended_frames.append(blended_frame)
 
     return np.array(blended_frames)
 
-
-def save_output_video(output_video_path, output_frames, fps):
+def save_output_video(output_video_path, output_frames, fps, width, height):
     """
     Save the output video.
     """
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Use 'mp4v' for MP4 format
-    out = cv2.VideoWriter(output_video_path, fourcc, fps, (WIDTH, HEIGHT))
-    for _, frame in enumerate(output_frames):
+    out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
+    for frame in output_frames:
         out.write(frame)
-
     out.release()
-
-
 
 if __name__ == "__main__":
     # Read data from the config
@@ -70,38 +66,28 @@ if __name__ == "__main__":
     mp_face_detection = mp.solutions.face_detection.FaceDetection(min_detection_confidence=0.5)
     mp_drawing = mp.solutions.drawing_utils
 
-    # Initial capture to create the first video (not yet a composite)
-    current_composite_frames = capture_frames(frame_count, WIDTH, HEIGHT)
+    # Initial capture to create the first video (at higher resolution)
+    current_composite_frames = capture_frames(picam2, frame_count, WIDTH, HEIGHT)
 
     # Generate a unique filename using a timestamp and save the video
     initial_video_filename = os.path.join(PLAY_DIR, f"{datetime.now().strftime('%Y%m%d_%H%M%S')}.mp4")
-    save_output_video(initial_video_filename, current_composite_frames, FPS)
+    save_output_video(initial_video_filename, current_composite_frames, FPS, WIDTH, HEIGHT)
     print(f"Initial video saved as {initial_video_filename}\n\n\n")
 
     with open("_completed_video.txt", "w") as f:
         f.write(initial_video_filename)
 
-
     # Begin the main loop
     while True:
-        # Initialize the Picamera2 with face detection resolution (640 x 480)
-        picam2 = Picamera2()
-        picam2.configure(picam2.create_still_configuration(main={"size": (640, 480), "format": "RGB888"}))
-        picam2.start()
-
         detection_score_1, detection_score_2 = 0, 0
         loop_start_time = t.time()
 
-        # Capture two frames for face detection (temporal filtering) and resize them.
-        width_detection = int(WIDTH / 2)
-        height_detection = int(HEIGHT / 2)
+        # Capture frame_1 and frame_2 at lower resolution (640 x 480)
+        picam2.configure(picam2.create_still_configuration(main={"size": (640, 480), "format": "RGB888"}))
+        picam2.start()
         frame_1 = picam2.capture_array()
-        frame_1 = cv2.resize(frame_1, (width_detection, height_detection))
         t.sleep(0.5)
         frame_2 = picam2.capture_array()
-        frame_2 = cv2.resize(frame_1, (width_detection, height_detection))
-
-        # Stop the picamera so that the following code can capture in the frame resolution.
         picam2.stop()
 
         # Detect faces
@@ -110,17 +96,15 @@ if __name__ == "__main__":
 
         # Get the detection scores
         if results_1.detections:
-            for detection in results_1.detections:
-                detection_score_1 = detection.score[0]
-                if DEBUG:
-                    mp_drawing.draw_detection(frame_1, detection)
+            detection_score_1 = results_1.detections[0].score[0]
+            if DEBUG:
+                mp_drawing.draw_detection(frame_1, results_1.detections[0])
 
         if results_2.detections:
-            for detection in results_2.detections:
-                detection_score_2 = detection.score[0]
-                if DEBUG:
-                    mp_drawing.draw_detection(frame_2, detection)
-        
+            detection_score_2 = results_2.detections[0].score[0]
+            if DEBUG:
+                mp_drawing.draw_detection(frame_2, results_2.detections[0])
+
         # Display the images if DEBUG is enabled.
         if DEBUG:
             cv2.imshow("main debug", frame_1)
@@ -132,34 +116,24 @@ if __name__ == "__main__":
             print(f"Results 2: {detection_score_2}")
             print("*********************")
 
-
         # If there are faces, overlay the frames.
         if (detection_score_1 > CONFIDENCE_THRESHOLD) and (detection_score_2 > CONFIDENCE_THRESHOLD):
             print("Face detected!")
 
-            # Capture new frames
-            capture_start_time = t.time()
-            new_frames = capture_frames(frame_count, WIDTH, HEIGHT)
-            capture_end_time = t.time()
-            print(f"Time for frame capture: {capture_end_time - capture_start_time:.4f}")
+            # Capture new frames at the original resolution (WIDTH x HEIGHT)
+            new_frames = capture_frames(picam2, frame_count, WIDTH, HEIGHT)
 
             # Perform alpha blending on all frames
-            blend_start_time = t.time()
             blended_frames = alpha_blend_frames(new_frames, current_composite_frames, ALPHA)
-            blend_end_time = t.time()
-            print(f"Time for frame blending: {blend_end_time - blend_start_time:.4f} seconds")
 
             # Update the current composite frames
             current_composite_frames = blended_frames
 
             # Generate a unique filename using a timestamp
-            video_save_start_time = t.time()
             new_video_filename = os.path.join(PLAY_DIR, f"_play_video_{datetime.now().strftime('%Y%m%d_%H%M%S')}.mp4")
             
             # Save the new composite as the play video
-            save_output_video(new_video_filename, current_composite_frames, FPS)
-            video_save_end_time = t.time()
-            print(f"Time for saving video: {video_save_end_time - video_save_start_time:.4f}")
+            save_output_video(new_video_filename, current_composite_frames, FPS, WIDTH, HEIGHT)
             print(f"  Updated video saved as {new_video_filename}")
 
             with open("_completed_video.txt", "w") as f:
@@ -174,7 +148,6 @@ if __name__ == "__main__":
             loop_end_time = t.time()
             print(f"Loop iteration completed in {loop_end_time - loop_start_time:.4f} seconds")
             print("--------------------------------------------")
-
 
         else:
             cv2.waitKey(int(1000 / FPS))
